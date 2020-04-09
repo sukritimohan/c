505ade070db6f247cd887330ecaa2b493e1e2c5e
sukritimohan <43717492+sukritimohan@users.noreply.github.com>
2020-04-09 23:06:03 +0700

    created exchange.c
    swaps two numbers.

---
+++ exchange.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int a,b;
+   printf("Enter no a:");
+   scanf("%d",&a);
+   printf("Enter no b:");
+   scanf("%d",&b);
+   printf("a=%d \n b=%d",b,a);
+   return 0;
+}
